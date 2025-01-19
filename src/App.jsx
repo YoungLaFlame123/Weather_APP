4f0c3b0e4107d3a277c2e512f1cf341c509c3fdc
YoungLaFlame123 <alycisse098@gmail.com>
2025-01-19 22:05:03 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Weather from './components/Weather'
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>Weather App using OpenWeatherMap API and React JS </h1> 
+      <Weather/>
+    </div>
+    
+  )
+}
+
+export default App
